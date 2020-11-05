0e49cc38595fc33111acb39f6c6c9744c55d3dff
Akimberl <mr.burbu.kb@gmail.com>
2020-11-05 14:08:13 +0300

    release 4

--- routes/profile.js
+++ routes/profile.js
@@ -10,16 +10,19 @@ const CheckUser = (req, res, next) => {
   next();
 }
 
+/* getting profile page */
 router.get('/', CheckUser, async (req, res, next) => {
   const auctions = await Auction.find({author: res.locals?.user._id }) // looking auction of user who logged in
   res.render('profile', { auctions, user: res.locals?.user });
 });
 
+/* getting form to add auction */
 router.get('/auc', CheckUser, async (req, res, next) => {
   res.render('itemsform', { user: res.locals?.user });
 });
 
-router.post('/auc', CheckUser, async (req, res, next) => {
+/* saving new auction */
+router.post('/auc', CheckUser, async (req, res) => {
   const auction = new Auction({
     name: req.body.name,
     author: res.locals.user._id,
@@ -29,6 +32,7 @@ router.post('/auc', CheckUser, async (req, res, next) => {
     description: req.body.description,
   });
 
+  /* saving obj to mongodb and returning status */
   auction.save()
     .then(() => {
       console.log('saved');
@@ -38,4 +42,31 @@ router.post('/auc', CheckUser, async (req, res, next) => {
       res.status(401).end();
     })
 });
+
+/* getting form to update auction */
+router.get('/auc/:id', async (req, res) => {
+  const auction = await Auction.findOne({_id: req.params.id}); // taking this obj so it renders with previous values
+  res.render('itemsform', {auction});
+});
+
+/* Updating */
+router.patch('/:id', (req, res) => {
+  // updating auction
+  Auction.findOneAndUpdate({_id: req.params.id}, {$set: {
+    name: req.body.name,
+    condition: req.body.condition,
+    startsAt: req.body.startsAt,
+    endsAt: req.body.endsAt,
+    description: req.body.description,
+  }})
+    .then(() => {
+      console.log('updated');
+      res.send(200).redirect('/profile');
+    })
+    .catch(() => {
+      console.log('could not update');
+      res.send(401).redirect('/profile');
+    })
+});
+
 module.exports = router;
